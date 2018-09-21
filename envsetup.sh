9dd4e2da552f590b588a9b5c721b5929a8e8f4a5
shinobizero <shinobizero@gmx.com>
2018-09-21 16:10:08 +1000

    Removed roomservice stuff from envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -681,20 +681,7 @@ function lunch()
 
     local product=$(echo -n $selection | sed -e "s/-.*$//")
     check_product $product
-    if [ $? -ne 0 ]
-    then
-        # if we can't find a product, try to grab it off the CM github
-        T=$(gettop)
-        cd $T > /dev/null
-        vendor/zero/build/tools/roomservice.py $product
-        cd - > /dev/null
-        check_product $product
-    else
-        T=$(gettop)
-        cd $T > /dev/null
-        vendor/zero/build/tools/roomservice.py $product true
-        cd - > /dev/null
-    fi
+
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     build_build_var_cache
